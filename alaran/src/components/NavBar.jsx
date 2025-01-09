acfb02ce1f6d09c700b0277f7a6b8ed3d41d698f
mrauthentik <umokeuchenna2020@gmail.com>
2025-01-09 18:32:37 +0100

    feat: add Font Awesome icons and update NavBar component layout

--- alaran/src/components/NavBar.jsx
+++ alaran/src/components/NavBar.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const NavBar = () => {
   return (
     <div>
-      
+       <nav>
+        <div className="logo"> LOGO</div>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+        <div className="icons">
+            <i className="fas fa-search"></i>
+            <i className="fas fa-shopping-cart"></i>
+        </div>
+       </nav>
     </div>
   )
 }
