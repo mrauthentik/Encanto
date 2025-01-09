dcf9aa9e5bad4a5e1ae81133faecfefe8941c441
mrauthentik <umokeuchenna2020@gmail.com>
2025-01-09 20:42:02 +0100

    style: enhance About section layout; add "Why Choose Us" heading and adjust main content styling

--- alaran/src/components/About.jsx
+++ alaran/src/components/About.jsx
@@ -5,10 +5,18 @@ const About = () => {
        <h2> About Us</h2>
         <main>
             <p> 
-                Alaran is a fashion brand that offers a wide range of clothing and accessories for <br />
+                Alaran is a fashion brand that offers a wide range of clothing and accessories for 
                 Alaran Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque numquam debitis earum consequuntur magnam adipisci esse soluta deleniti? Reiciendis voluptatum temporibus unde suscipit. Dolorem error aliquam tenetur saepe reiciendis facilis!
             </p>
         </main>
+        <h2 className="why-choose-us">Why Choose Us</h2>
+        <main>
+            <p> 
+                Alaran is a fashion brand that offers a wide range of clothing and accessories for 
+                Alaran Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque numquam debitis earum consequuntur magnam adipisci esse soluta deleniti? Reiciendis voluptatum temporibus unde suscipit. Dolorem error aliquam tenetur saepe reiciendis facilis!
+            </p>
+
+            </main>
     </section>
   )
 }
