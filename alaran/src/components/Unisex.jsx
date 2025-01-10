1537cab59d0c9ce695bdf8e0f3952cef74367a55
mrauthentik <umokeuchenna2020@gmail.com>
2025-01-10 13:22:25 +0100

    feat: enhance Unisex component with images and layout adjustments

--- alaran/src/components/Unisex.jsx
+++ alaran/src/components/Unisex.jsx
@@ -1,8 +1,19 @@
+import image1 from '../assets/fashion (9).jpg'
+import image2 from '../assets/fashion (12).jpg'
 
 const Unisex = () => {
   return (
     <div>
-      Unisex will display here
+      <main className='unisex'>
+        <h2>Unisex</h2>
+        <div className="side">
+          Alaran <i>for the culture...</i>
+        </div>
+          <div className="unisex-images">
+              <img src={image1} className='unisex-img1' alt="" />
+              <img src={image2} className='unisex-img2' alt="" />
+          </div>
+      </main>
     </div>
   )
 }
