0e2cbd95269e5ef3ee4321ea3c7fb6d5e09e95a6
mrauthentik <umokeuchenna2020@gmail.com>
2025-01-20 18:09:12 +0100

    Add Landing and About components to AboutPage for improved structure

--- alaran/src/components/AboutPage.jsx
+++ alaran/src/components/AboutPage.jsx
@@ -1,10 +1,14 @@
 // Code for the About Page
 import NavBar from './NavBar'
+import Landing from './Landing'
+import About from './About'
 
 const AboutPage = () => {
   return (
     <div>
       <NavBar />
+      <Landing />
+        <About />
       <h1>Welcome to About us Page</h1>
     </div>
   )
