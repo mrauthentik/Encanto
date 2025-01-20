24e98849ecd02ae775dc355e2aa952354b51dd6b
mrauthentik <umokeuchenna2020@gmail.com>
2025-01-20 19:21:56 +0100

    Refactor AboutPage component to remove unused prop and enhance content with project description

--- alaran/src/components/AboutPage.jsx
+++ alaran/src/components/AboutPage.jsx
@@ -3,13 +3,19 @@ import NavBar from './NavBar'
 import Landing from './Landing'
 import About from './About'
 
-const AboutPage = ({text}) => {
+const AboutPage = () => {
   return (
     <div>
       <NavBar />
       <Landing />
         <About />
-      <h1>Welcome to About us Page {text}</h1>
+         <main>
+            <h2> THE ALARAN PROJECT</h2>
+            <p>
+            Our journey began as we decided to pay attention to the Nigeria textile markets, providing several indigenously made fabric designs that can compete in the global market. 
+            <br />
+            The Alaran project sees to create outstanding luxury clothing designs that give you the confidence and also promote the Africa culture, making you standout anywhere and everywhere in the world. We also intend to in the process, grow our country’s economic power as we join in the ‘buy Nigeria, grow Nigeria’ campaign as we also join to improve the Africa market at large.</p>
+         </main>
     </div>
   )
 }
