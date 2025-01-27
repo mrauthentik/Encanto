0151c4d883617b9b143e1598e01ff4f41538c9ee
mrauthentik <umokeuchenna2020@gmail.com>
2025-01-27 14:43:17 +0100

    Add navigation link to Design component in Male.jsx for improved user experience

--- alaran/src/components/Male.jsx
+++ alaran/src/components/Male.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { easeIn, motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
@@ -68,9 +69,11 @@ const Male = () => {
             <div className="product-info">
               <h3>{product.title}</h3>
               <p>{product.price}</p>
+              <Link to='/design' >
               <button className="shop-now-btn">
-                <FontAwesomeIcon icon={faShoppingCart} /> Shop Now
+                <FontAwesomeIcon icon={faShoppingCart} />  Shop Now
               </button>
+              </Link>
             </div>
           </motion.div>
         ))}
