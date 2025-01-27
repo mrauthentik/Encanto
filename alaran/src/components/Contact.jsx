e3988dec475032f7e4c5c2a162bbafd46ab21627
mrauthentik <umokeuchenna2020@gmail.com>
2025-01-27 12:18:43 +0100

    Add contact information section with icons and styling; enhance layout for better user engagement

--- alaran/src/components/Contact.jsx
+++ alaran/src/components/Contact.jsx
@@ -1,7 +1,7 @@
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight,  faClockRotateLeft, faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
@@ -36,6 +36,7 @@ const Contact = () => {
         </div>
       </motion.div>
       <main>
+      <h2>CONTACT US</h2>
         {/* Animated iframe */}
         <motion.iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d504009.81876169617!2d6.7690487890625!3d9.282060000000012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104ddd1171bf298b%3A0x240f035438cf6898!2sOld%20Soldiers%20Quarters%2C%20Sabon-Gari%20Extension%20Bwari%20Abuja.!5e0!3m2!1sen!2sng!4v1737881409237!5m2!1sen!2sng"
@@ -58,7 +59,31 @@ const Contact = () => {
           initial="initial"
           animate="animate"
         >
-          <h2>CONTACT US</h2>
+             <div className="contact-blocks">
+              <div className="contact-block">
+                  <FontAwesomeIcon icon={faLocationDot} className="contact-icon" />
+                  <h5> Our Location</h5>
+                   <p>We are located at No 10, Old soldier Quarters, FCT Abuja.</p>
+              </div>
+              
+              <div className="contact-block">
+                  <FontAwesomeIcon icon={faPhone} className="contact-icon" />
+                  <h5> Contact Info</h5>
+                   <p> 
+                    <a href="mailto:alaranafrica@gmail.com">Email: alaranafrica@gmail.com</a>
+                   <br /> <a href="tel:+234 806 661 8494">Telephone: +234 806 661 8494 </a>
+                    </p>
+              </div>
+              
+              <div className="contact-block">
+                  <FontAwesomeIcon icon={faClockRotateLeft} className="contact-icon" />
+                  <h5> Business Hours</h5>
+                   <p>Monday - Saturday: <br />
+                   09:00 am - 20:00 pm.</p>
+              </div>
+              
+             </div>
+          
           <p className="contact-text">
             In one click, reach out to us for custom designs, complaints, and
             recommendations. Your satisfaction is our concern.
